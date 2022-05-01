373ed8e0b797d5b0f338e319e5d76675680adac4
murata0531 <coless115@gmail.com>
2022-05-01 14:14:33 +0900

    送信ボタン押下で新しくtodoを追加する

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -1,23 +1,56 @@
 // React から useState フックをインポート
 import { useState } from 'react';
 
+type Todo = {
+  value: string;
+};
+
 export const App = () => {
 
   const [text, setText] = useState('');
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  // todos ステートを更新する関数
+  const handleOnSubmit = () => {
+    // 何も入力されていなかったらリターン
+    if (!text) return;
+
+    // 新しい Todo を作成
+    const newTodo: Todo = {
+      value: text,
+    };
+    /**
+     * スプレッド構文を用いて todos ステートのコピーへ newTodo を追加する
+     * 以下と同義
+     *
+     * const oldTodos = todos.slice();
+     * oldTodos.unshift(newTodo);
+     * setTodos(oldTodos);
+     *
+     **/
+    setTodos([newTodo, ...todos]);
+    // フォームへの入力をクリアする
+    setText('');
+  };
 
   return (
     <div>
-      <form onSubmit={(e) => e.preventDefault()}>
+      <form onSubmit={(e) => {
+          e.preventDefault();
+          handleOnSubmit();
+        }}
+      >
         <input
           type="text"
           value={text}
           onChange={(e) => setText(e.target.value)}
         />
         <input
-          type="submit"
-          value="追加"
-          onSubmit={(e) => e.preventDefault()}
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
         />
+        <input type="submit" value="追加" onSubmit={handleOnSubmit} />
       </form>
     </div>
   );
