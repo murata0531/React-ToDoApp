35a03b73b5ef4a4d0e7e65bb2b1824307158df41
murata0531 <coless115@gmail.com>
2022-05-05 00:01:10 +0900

    タスクのステートを更新するアクション

---
+++ todo/src/@types/Action.d.ts
@@ -0,0 +1,9 @@
+// タスクのステートを更新するアクション
+declare type Action =
+  | { type: 'change'; text: string }
+  | { type: 'filter'; filter: Filter }
+  | { type: 'submit' }
+  | { type: 'empty' }
+  | { type: 'edit'; id: number; value: string }
+  | { type: 'check'; id: number; checked: boolean }
+  | { type: 'remove'; id: number; removed: boolean };
