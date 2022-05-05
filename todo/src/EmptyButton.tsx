be24f8a4c273069e2b87f9a6e19802a27c168945
murata0531 <coless115@gmail.com>
2022-05-05 22:22:18 +0900

    ゴミ箱を空にするコンポーネント

---
+++ todo/src/EmptyButton.tsx
@@ -0,0 +1,16 @@
+// ゴミ箱をからにするコンポーネント
+import { Dispatch, memo } from 'react';
+
+type Props = {
+  dispatch: Dispatch<Action>;
+};
+
+export const EmptyButton = memo((props: Props) => {
+  const handleOnEmpty = () => {
+    props.dispatch({ type: 'empty' });
+  };
+
+  return <button onClick={handleOnEmpty}>ごみ箱を空にする</button>;
+});
+
+EmptyButton.displayName = 'EmptyButton';
