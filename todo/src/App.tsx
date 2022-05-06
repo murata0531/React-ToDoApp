2ab90777f81ac409a0ef56d114671b38a25993d1
murata0531 <coless115@gmail.com>
2022-05-06 18:27:09 +0900

    stateをreducerに置き換え

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -1,115 +1,25 @@
-// React から useState フックをインポート
-// import { useState } from 'react';
 import { useReducer } from 'react';
+
 import { reducer } from './reducer';
 import { initialState } from './initialState';
 
-export const App = () => {
+import { Form } from './Form';
+import { Selector } from './Selector';
+import { EmptyButton } from './EmptyButton';
+import { FilteredTodos } from './FilteredTodos';
 
+export const App = (): JSX.Element => {
   const [state, dispatch] = useReducer(reducer, initialState);
-
-  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    dispatch({ type: 'change', text: e.target.value });
-  };
-
-  const handleOnSubmit = () => {
-    dispatch({ type: 'submit' });
-  };
-
-  const handleOnEdit = (id: number, value: string) => {
-    dispatch({ type: 'edit', id, value });
-  };
-
-  const handleOnCheck = (id: number, checked: boolean) => {
-    dispatch({ type: 'check', id, checked });
-  };
-
-  const handleOnRemove = (id: number, removed: boolean) => {
-    dispatch({ type: 'remove', id, removed });
-  };
-
-  const handleOnEmpty = () => {
-    dispatch({ type: 'empty' });
-  };
-
-  const handleOnFilter = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    dispatch({ type: 'filter', filter: e.target.value as Filter });
-  };
-  
-  const filteredTodos = state.todos.filter((todo) => {
-    switch (state.filter) {
-      case 'all':
-        return !todo.removed;
-      case 'checked':
-        return todo.checked && !todo.removed;
-      case 'unchecked':
-        return !todo.checked && !todo.removed;
-      case 'removed':
-        return todo.removed;
-      default:
-        return todo;
-    }
-  });
 
   return (
     <div>
-      <select defaultValue="all" onChange={handleOnFilter}>
-        <option value="all">すべてのタスク</option>
-        <option value="checked">完了したタスク</option>
-        <option value="unchecked">現在のタスク</option>
-        <option value="removed">ごみ箱</option>
-      </select>
+      <Selector dispatch={dispatch} />
       {state.filter === 'removed' ? (
-        <button
-          onClick={handleOnEmpty}
-          disabled={state.todos.filter((todo) => todo.removed).length === 0}
-        >
-          ゴミ箱を空にする
-        </button>
+        <EmptyButton dispatch={dispatch} />
       ) : (
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            handleOnSubmit();
-          }}
-        >
-          <input
-            type="text"
-            value={state.text}
-            disabled={state.filter === 'checked'}
-            onChange={(e) => handleOnChange(e)}
-          />
-          <input
-            type="submit"
-            value="追加"
-            disabled={state.filter === 'checked'}
-            onSubmit={handleOnSubmit}
-          />
-        </form>
+        <Form state={state} dispatch={dispatch} />
       )}
-      <ul>
-        {filteredTodos.map((todo) => {
-          return (
-            <li key={todo.id}>
-              <input
-                type="checkbox"
-                disabled={todo.removed}
-                checked={todo.checked}
-                onChange={() => handleOnCheck(todo.id, todo.checked)}
-              />
-              <input
-                type="text"
-                disabled={todo.checked || todo.removed}
-                value={todo.value}
-                onChange={(e) => handleOnEdit(todo.id, e.target.value)}
-              />
-              <button onClick={() => handleOnRemove(todo.id, todo.removed)}>
-                {todo.removed ? '復元' : '削除'}
-              </button>
-            </li>
-          );
-        })}
-      </ul>
+      <FilteredTodos state={state} dispatch={dispatch} />
     </div>
   );
 };
