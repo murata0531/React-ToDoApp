30dc00df1f6bf05ddf378106b045c8e52db60651
overmurata <naoki.murata@spread-over.co.jp>
2022-12-12 11:28:50 +0900

    バグ修正

--- laravel_src/app/Models/User.php
+++ laravel_src/app/Models/User.php
@@ -18,9 +18,9 @@ class User extends Authenticatable
      * @var string[]
      */
     protected $fillable = [
-        'username',
-        // 'email',
-        // 'password',
+        'name',
+        'email',
+        'password',
     ];
 
     /**
