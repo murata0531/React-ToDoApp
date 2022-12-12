67ba9afd8959e159e5db520a2496096c3b3f4858
overmurata <naoki.murata@spread-over.co.jp>
2022-12-12 10:13:16 +0900

    ルートを追加

--- todo/src/main.tsx
+++ todo/src/main.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Route, BrowserRouter ,Routes } from "react-router-dom";
 import { App } from './App';
+import Sample  from './sample';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/todo" element={<App />} />
+        <Route path="/sample" element={<Sample />} />
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
